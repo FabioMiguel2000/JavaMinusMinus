923adea835e48c49522e52d97f24d4236bb9cea0
João Bispo <joaobispo@gmail.com>
2022-05-03 20:09:38 +0100

    Adds new stage, AstToJasmin, for groups that will convert the AST directly to Jasmin
    - New interface AstToJasmin, that should be registered in config.properties with the key 'AstToJasminClass'
    - Overload for TestUtils.backend() that receives a JmmSemanticsResult
    - TestUtils.backend() overloads that receive a String of Java-- code call AstToJasmin if property has a  value in file config.properties 
    - New protected method AJmmVisitor.visitAllChildren(), which can be used as a default visit method
    - Improves error message when the class name of a stage is empty in the config.properties file

--- src-lib/pt/up/fe/comp/jmm/ollir/JmmOptimization.java
+++ src-lib/pt/up/fe/comp/jmm/ollir/JmmOptimization.java
@@ -2,11 +2,10 @@ package pt.up.fe.comp.jmm.ollir;
 
 import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
 
-
 /**
  * This Stage deals with optimizations performed at the AST level and at the OLLIR level.<br>
- * Note that for Checkpoint 2 (CP2) only the @{JmmOptimization#toOllir} has to be developed. The other two methods are
- * for Checkpoint 3 (CP3).
+ * Note that for Checkpoint 2 (CP2) only the {@link JmmOptimization#toOllir(JmmSemanticsResult)} has to be developed.
+ * The other two methods are for Checkpoint 3 (CP3).
  */
 public interface JmmOptimization {
 
@@ -31,7 +30,7 @@ public interface JmmOptimization {
     /**
      * Step 3 (for CP3): optimize code at the OLLIR level
      * 
-     * @param ollirResult
+     * @param semanticsResult
      * @return
      */
     default OllirResult optimize(OllirResult ollirResult) {
