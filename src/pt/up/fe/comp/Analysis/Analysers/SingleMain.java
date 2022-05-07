9cc00cf2e676332cca2b975d6407705bec920a5c
Gabriel <up201709532@fe.up.pt>
2022-05-07 17:27:01 +0100

    testes compiling successfully

--- src/pt/up/fe/comp/Analysis/Analysers/SingleMain.java
+++ src/pt/up/fe/comp/Analysis/Analysers/SingleMain.java
@@ -3,18 +3,19 @@ package pt.up.fe.comp.Analysis.Analysers;
 import pt.up.fe.comp.jmm.report.Report;
 import pt.up.fe.comp.jmm.report.ReportType;
 import pt.up.fe.comp.jmm.report.Stage;
-import pt.up.fe.comp.Analysis.JmmSemanticAnalyser;
+import pt.up.fe.comp.Analysis.JmmPreorderSemanticAnalyser;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Collections;
 
-public class SingleMain extends JmmPreorderSemanticAnalyser<Integer, Integer> {
+public class SingleMain extends JmmPreorderSemanticAnalyser {
 
     @Override
     public List<Report> getReports() {
 
         //return Arrays.asList(new Report(ReportType.ERROR, Stage.SEMANTIC, -1, -1, "random error"));
         //return Collections.emptyList();
+        return Collections.emptyList();
     }
 }
