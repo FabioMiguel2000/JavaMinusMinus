a8bee8e7390a944051cd1ac834b65dc033cecc17
Goloso98 <gooloso98@gmail.com>
2022-05-13 19:10:05 +0100

    New semantic testes methods.

--- test/AnalysisTest.java
+++ test/AnalysisTest.java
@@ -11,4 +11,28 @@ public class AnalysisTest {
         System.out.println("SymbolTable: \n" + result2.getSymbolTable().print());
         TestUtils.noErrors(result2);
     }
+
+    public void test_noErrors(String testLocation) {
+        var result = TestUtils.parse(SpecsIo.getResource(testLocation));
+        System.out.println(result.getRootNode().toTree());
+        var result2 = TestUtils.analyse(SpecsIo.getResource(testLocation)); // Calls JmmAnalyser semanticAnalysis()
+        System.out.println("SymbolTable: \n" + result2.getSymbolTable().print());
+        TestUtils.noErrors(result2);
+    }
+
+    public void test_mustFail(String testLocationFile) {
+        var testLocation = "myAnalysis/" + testLocationFile;
+        var result = TestUtils.parse(SpecsIo.getResource(testLocation));
+        System.out.println(result.getRootNode().toTree());
+        var result2 = TestUtils.analyse(SpecsIo.getResource(testLocation)); // Calls JmmAnalyser semanticAnalysis()
+        System.out.println("SymbolTable: \n" + result2.getSymbolTable().print());
+        TestUtils.mustFail(result2);
+    }
+
+    // Testes located inside myAnalysis folder
+    @Test
+    public void fail_test1() { test_mustFail("FindMaximum2.jmm"); }
+
+    @Test
+    public void pass_test1() { test_noErrors("FindMaximum.jmm"); }
 }
