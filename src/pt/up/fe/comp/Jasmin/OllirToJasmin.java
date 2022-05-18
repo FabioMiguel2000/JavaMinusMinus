a3d18bf240517eae53e2bc3bcd57753ac66c0afb
Fab <fabiomiguelchenhuang@gmail.com>
2022-05-18 21:42:42 +0100

    added ollir to jasmin for some instruction, not finished yet, OllirToJasminArithmeticTest passing

--- src/pt/up/fe/comp/Jasmin/OllirToJasmin.java
+++ src/pt/up/fe/comp/Jasmin/OllirToJasmin.java
@@ -6,13 +6,15 @@ import pt.up.fe.specs.util.SpecsIo;
 import pt.up.fe.specs.util.classmap.FunctionClassMap;
 import pt.up.fe.specs.util.exceptions.NotImplementedException;
 
+import java.util.HashMap;
 import java.util.stream.Collectors;
 
 public class OllirToJasmin {
     private final ClassUnit classUnit;
+    HashMap<String, Descriptor> varTable;
     public OllirToJasmin(ClassUnit classUnit){
         this.classUnit = classUnit;
-
+        classUnit.buildVarTables();
     }
 
     public String getFullyQualifiedName(String className){
@@ -35,24 +37,31 @@ public class OllirToJasmin {
     }
 
     public String getCode(){
-        System.out.println("estou aqui");
         var code = new StringBuilder();
 
         code.append(".class public ").append(classUnit.getClassName()).append("\n");
 
-        var superClassQualifiedName = getFullyQualifiedName(classUnit.getSuperClass());
+//        System.out.println("current code = " + code);
+        String superClassQualifiedName;
+        code.append(".super ");
+        if(classUnit.getSuperClass() == null){
+            superClassQualifiedName = "java/lang/Object";
+            code.append(superClassQualifiedName).append("\n");
+        }
+        else{
+            superClassQualifiedName = getFullyQualifiedName(classUnit.getSuperClass());
+            code.append(classUnit.getSuperClass()).append("\n");
+        }
 
-        code.append(".super ").append(classUnit.getSuperClass()).append("\n");
+
 
         code.append(SpecsIo.getResource("templates/JasminConstructor.template").replace("${SUPER_NAME}", superClassQualifiedName)).append("\n");
 
-
-        // TODO: Fields  using classUnit.getFields()
-
-
-        for(var field: classUnit.getFields()){
-            code.append(getCode(field));
-        }
+//        for(var field: classUnit.getFields()){
+//            // TODO: Fields  using classUnit.getFields()
+//
+//            code.append(getCode(field));
+//        }
 
         for (var method: classUnit.getMethods()){
             code.append(getCode(method));
@@ -63,12 +72,19 @@ public class OllirToJasmin {
     }
     public String getCode(Field field){
 
-        System.out.println("Entrou field");
         return "";
     }
 
     public String getCode(Method method){
         var code = new StringBuilder();
+
+        this.varTable = method.getVarTable();
+
+        System.out.println("VarTable:" + method.getVarTable());
+
+        if(method.isConstructMethod()){
+            return "";
+        }
 
         code.append(".method ");
 
@@ -101,7 +117,7 @@ public class OllirToJasmin {
                 .map(element -> getJasminType(element.getType()))
                 .collect(Collectors.joining());
 
-        System.out.println("METHOD PARAMS: " + methodParamTypes);
+//        System.out.println("METHOD PARAMS: " + methodParamTypes);
 
         code.append(methodParamTypes).append(")").append(getJasminType(method.getReturnType())).append("\n");
         code.append(".limit stack 99\n");
@@ -109,42 +125,186 @@ public class OllirToJasmin {
 
 
         for(var instruction: method.getInstructions()){
-            code.append(getCode(instruction));
+            code.append(getInstructionCode(instruction, method));
         }
-
-        code.append("return\n");
+//        if(method.getMethodName().equals("main")){
+//            code.append("return\n");
+//
+//        }
         code.append(".end method\n\n");
         return code.toString();
 
     }
-    public String getCode(Instruction instruction){
+    public String getInstructionCode(Instruction instruction, Method method){
+        instruction.show();
 
-        // return instructionMap.apply(instruction);
-        if(instruction instanceof CallInstruction){
-            return getCode((CallInstruction) instruction);
-        }
-        if(instruction instanceof AssignInstruction){
-            return getCode((AssignInstruction)instruction);
-        }
-        if(instruction instanceof OpInstruction){
-            return getCode((OpInstruction) instruction);
-        }
+//         return instructionMap.apply(instruction);
+//        if(instruction instanceof CallInstruction){
+//            return getCode((CallInstruction) instruction);
+//        }
+//        if(instruction instanceof AssignInstruction){
+//            return getAssignInstructionCode((AssignInstruction)instruction, method);
+//        }
 
-        // ... TODO: other instructions, maybe use switch
-        return "";
-    }
-    public String getCode(AssignInstruction assignInstruction){
-        System.out.println("ASSIGN");
-//        assignInstruction.show();
-//        switch (assignInstruction.getTypeOfAssign().getTypeOfElement()){
+//        if(instruction instanceof OpInstruction){
 //
 //        }
 
-        return "";
-    }
-    public String getCode(OpInstruction opInstruction){
-        System.out.println("OP");
+        switch (instruction.getInstType()){
+            case NOPER:
+                return getNOPERCode((SingleOpInstruction) instruction, method);
 
+            case CALL:
+                return getCode((CallInstruction) instruction);
+//            case GOTO:
+
+            case ASSIGN:
+                return getAssignInstructionCode((AssignInstruction)instruction, method);
+            case RETURN:
+                return getReturnInstructionCode((ReturnInstruction)instruction, method);
+//            case BRANCH:
+//                case RETURN:
+//            case GETFIELD:
+//            case PUTFIELD:
+//            case UNARYOPER:
+//                return getCode((OpInstruction) instruction);
+            case BINARYOPER:
+                return getBinaryOpInstructionCode((BinaryOpInstruction)instruction, method);
+//                return getCode((OpInstruction) instruction);
+            default:
+                throw new NotImplementedException(instruction.getInstType());
+        }
+
+//        return "";
+    }
+    public String getReturnInstructionCode(ReturnInstruction returnInstruction, Method parentMethod){
+        if(!returnInstruction.hasReturnValue()){
+            return "return\n";
+        }
+
+        switch (returnInstruction.getOperand().getType().getTypeOfElement()){
+            case VOID:
+                return "return\n";
+            case INT32, BOOLEAN:
+                return loadElement(returnInstruction.getOperand()) + "ireturn\n";
+            case ARRAYREF, OBJECTREF:
+                return loadElement(returnInstruction.getOperand()) + "areturn\n";
+            default:
+                throw new NotImplementedException(returnInstruction.getOperand().getType().getTypeOfElement());
+        }
+    }
+    public String getNOPERCode(SingleOpInstruction instruction, Method parentMethod){
+        return loadElement(instruction.getSingleOperand());
+    }
+    public String getBinaryOpInstructionCode(BinaryOpInstruction binaryOpInstruction, Method method){
+        StringBuilder code = new StringBuilder();
+        code.append(loadElement(binaryOpInstruction.getLeftOperand()));
+        code.append(loadElement(binaryOpInstruction.getRightOperand()));
+
+        switch (binaryOpInstruction.getOperation().getOpType()){
+            case ADD:
+                code.append("iadd\n");
+                break;
+            case SUB:
+                code.append("isub\n");
+                break;
+
+            case MUL:
+                code.append("imul\n");
+                break;
+            case DIV:
+                code.append("idiv\n");
+                break;
+            case AND:
+                code.append("TODO:AND_NOT_IMPLEMENTED\n");
+                break;
+            case LTH:
+                code.append("TODO:LTH_NOT_IMPLEMENTED\n");
+                break;
+            default:
+                throw new NotImplementedException(binaryOpInstruction.getOperation().getOpType());
+        }
+
+        return code.toString();
+    }
+
+    public String loadElement(Element element){
+
+
+//        System.out.println("Inside NOPER: ");
+//        instruction.show();
+
+
+//        Element singleOperandElement = instruction.getSingleOperand();
+        if(element instanceof ArrayOperand){
+            StringBuilder code = new StringBuilder();
+            ArrayOperand operand = (ArrayOperand) element;
+
+            code.append("aload ").append(getVirtualRegister(operand.getName())).append("\n");
+
+            code.append(loadElement(operand.getIndexOperands().get(0)));
+
+            return code + "iaload\n";
+        }
+        if(element.isLiteral()){
+            String literalString = ((LiteralElement) element).getLiteral();
+            return loadIntToStack(literalString);
+        }
+        if(element instanceof Operand){
+            Operand operand = (Operand) element;
+            switch(operand.getType().getTypeOfElement()){
+                case THIS:
+                    return "aload_0\n";
+                case INT32, BOOLEAN:
+                    return "iload"+ getVirtualRegister(operand.getName()) + "\n";
+                case OBJECTREF, ARRAYREF:
+                    return "aload" + getVirtualRegister(operand.getName()) + "\n";
+                default:
+                    throw new NotImplementedException(operand);
+            }
+        }
+
+        throw new NotImplementedException(element);
+    }
+
+
+
+    public String getAssignInstructionCode(AssignInstruction assignInstruction, Method parentMethod){
+
+        parentMethod.buildVarTable();
+        this.varTable = parentMethod.getVarTable();
+
+        StringBuilder code = new StringBuilder();
+
+        Operand operand = (Operand) assignInstruction.getDest(); // left hand side of the expression ' THIS_PART = 1+2*2 '
+
+        if(operand instanceof ArrayOperand){
+            // left hand side is an array type
+        }
+
+        assignInstruction.getRhs().show();
+
+        code.append(getInstructionCode(assignInstruction.getRhs(), parentMethod)); // append the right hand side instruction code first
+
+
+        if(!(operand.getType().getTypeOfElement().equals(ElementType.OBJECTREF)
+                && assignInstruction.getRhs() instanceof CallInstruction)) {
+            code.append(storeValueIntoVariable(operand));
+        }
+
+        return code.toString();
+    }
+
+
+    public String getCode(OpInstruction opInstruction){
+
+        if(opInstruction instanceof UnaryOpInstruction){
+
+
+        }
+        if(opInstruction instanceof BinaryOpInstruction){
+
+        }
 //        opInstruction.show();
 //        switch (assignInstruction.getTypeOfAssign().getTypeOfElement()){
 //
@@ -187,6 +347,7 @@ public class OllirToJasmin {
         var methodClass = ((Operand) callInstruction.getFirstArg()).getName();
 
         code.append(getFullyQualifiedName(methodClass));
+
         code.append("/");
         var calledMethod = ((LiteralElement) callInstruction.getSecondArg()).getLiteral();
         code.append(calledMethod.substring(1, calledMethod.length()-1));
@@ -248,5 +409,43 @@ public class OllirToJasmin {
         }
     }
 
+    // ------------------------- UTILS --------------------------------------
+    private String getVirtualRegister(String variableName){
+        int virtualRegister = this.varTable.get(variableName).getVirtualReg();
+        if(virtualRegister > 3){
+            return " " + virtualRegister;
+        }
+        return "_" + virtualRegister;
+    }
 
+    private String loadIntToStack(String literal){
+        if(Integer.parseInt(literal) <= 5 && Integer.parseInt(literal) >= 0){ // 0 -> 5
+            return "iconst_" + literal + "\n";
+        }
+        if(Integer.parseInt(literal) == -1 ){
+            return "iconst_m1"+ "\n";
+        }
+        if(Integer.parseInt(literal) <= 127 && Integer.parseInt(literal) >= -128){
+            return "bipush " + literal+ "\n";
+        }
+        if(Integer.parseInt(literal) <= 32767 && Integer.parseInt(literal) >= -327678){
+            return "sipush " + literal+ "\n";
+        }
+        return "ldc " + literal+ "\n";
+    }
+
+    private String storeValueIntoVariable(Operand operand){
+        if(operand instanceof ArrayOperand){
+            return "iastore\n";
+        }
+
+        switch(operand.getType().getTypeOfElement()){
+            case INT32, BOOLEAN:
+                return "istore" + getVirtualRegister(operand.getName()) + "\n";
+            case OBJECTREF, ARRAYREF:
+                return "astore" + getVirtualRegister(operand.getName())+ "\n";
+            default:
+                throw new NotImplementedException(operand);
+        }
+    }
 }
